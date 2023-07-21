f8accbc14e3e6591f771331b28fe9ce894705f5e
Austin Stephen <60085701+AustinStephen@users.noreply.github.com>
2023-07-21 23:42:27 +0000

    Austin's first commit

---
+++ coffee.py
@@ -0,0 +1,10 @@
+"""
+Authors: Austin and Natalie
+Purpose: Happy birthday Josh! 
+"""
+
+print("Josh's Coffee Application!")
+
+syrups = ["hazlenut", "lavendar", "vanilla"]
+
+coffeeType = ["Latte", "Cappuccino"]
